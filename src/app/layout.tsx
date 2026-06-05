9a8a6b14f598a572588a3c890f77c3993417ca6b
Aziz Setiawan <azizunsil@gmail.com>
2026-06-05 05:48:13 +0000

    Saya sudah menaruh file icon aplikasi di folder public:
    - android-chrom

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-
 import type { Metadata, Viewport } from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
@@ -9,6 +8,17 @@ export const metadata: Metadata = {
   title: 'Barang dan Roris',
   description: 'Pencatatan harga produk usaha kecil realtime.',
   generator: 'Next.js',
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: [
+      { url: '/apple-touch-icon.png' },
+    ],
+  },
+  manifest: '/site.webmanifest',
 };
 
 export const viewport: Viewport = {
